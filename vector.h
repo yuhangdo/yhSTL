2c9144a4a524cb43ca1b144667a418d1fd7d4186
yuhangdo yuhangdo <wangyuh111@163.com>
2022-09-19 00:47:03 +0800

    add vector::insert

--- vector.h
+++ vector.h
@@ -9,7 +9,7 @@
 
 //20220918  弄完vector，先弄基础的，比如全局的constructor和destroy等，还有copy等
 //注意一定要加simple_alloc这个类
-//20220918  23:18 开始今晚的项目
+//20220918  23:18 开始今晚的项目  实现了insert操作
 #include <iterator>
 namespace yhstl
 {
@@ -67,6 +67,7 @@ namespace yhstl
 		}
 		reference front() { return *begin(); }  //返回第一个元素
 		reference back() { return *(end() - 1); } //返回最后一个元素，思路是尾后迭代器-1,
+		void insert(iterator position,size_type n,const T&x);   //先声明，一会儿定义
 		void push_back(const T& x)   //将元素插入尾端
 		{
 			if (finish != end_of_storage)  //先判断当前分配空间是否有剩余
@@ -159,6 +160,71 @@ namespace yhstl
 		}
 
 	};
+	template <class T>
+	void vector<T>::insert(iterator position, size_type n, const T& x)
+	{
+		if (n != 0)   //当n不等于0时，才进行以下操作
+		{
+			if (size_type(end_of_storage - finish) >= 0)   //剩余空间足够
+			{
+				T x_copy = x;
+				//接着计算 position之后的现有元素个数
+				const size_type elems_after = finish - position;  //注意是const
+				iterator old_finish = finish;
+				if (elems_after > n)   //如果position之后的现有个数大于n
+				{	
+					//这个板块主要是先把[finish-elems_after，finish]的元素在finish+n构造
+					// 其次是把原vector的position+n到finish-n的元素用，copy_backward到position
+					//接下来这个copy函数是实现将在[finish,finish+n]区域构造出[finish-n,finish]的元素
+					uninitialized_copy(finish - n, finish, finish);  //注意这个函数要之后自己实现
+					finish += n;  //将finish这个迭代器更新
+					copy_backward(position, old_finish - n, old_finish);  //这个函数同样需要注意
+					fill(position, position + n, x_copy); //在位置上构造x
+				}
+				else    //n>elems_after
+				{
+					//插入点之后元素小于新增人数
+					uninitialized_fill_n(finish, n - elems_after, x_copy);//先在finish之后构造 n-elem个x
+					finish += n - elems_after;		//更新finish
+					uninitialized_copy(position, old_finish, finish);    //把原vector的position之后的元素copy到更新后的finish之后了
+					finish += elems_after;		//更新finish
+					fill(position, old_finish, x_copy);    //在position到old_finish中构造x
+				}
+			}
+			else   //备用空间<插入元素个数 ,要设计动态分配空间
+			{
+				const size_type old_size = size();
+				const size_type len = old_size + max(old_size, n); //首先决定是用2*old还是old+n，取大值
+				//接着配置新的空间
+				iterator new_start = data_allocator(len);  //先分配len个空间，然后定义一个newstart迭代值指向开辟空间的首地址
+				iterator new_finish = new_start; //vector动态扩展都需要定义两个新迭代器，并都初始指向新分配空间
+				try    //又需要进行异常处理了
+				{
+					//首先将旧vector position之前的元素复制到新vector中
+					new_finish = uninitialized_copy(start, position, new_start);
+					//将新增元素加入新vector中
+					new_finish = uninitialized_fill_n(new_finish, n, x);  //这个函数之后也要自己实现
+					//再将旧vector position之后的元素复制到新vector中
+					new_finish = uninitialized_copy(position, finish, new_finish);
+				}
+				catch (...)
+				{
+					//如有异常发生，就实现commit or rollback
+					destroy(new_start, new_finish); //先析构
+					data_allocator::deallocator(new_start, len);
+					throw;
+				}
+				//以下清除旧vector
+				destroy(start, finish);
+				deallocate();
+				//调整vector的三个迭代器
+				start = new_start;
+				finish = new_finish;
+				end_of_storage = new_start + len;
+
+			}
+		}
+	}
 
 
 
