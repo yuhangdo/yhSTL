380df55efbfd1762e535e3eab1c193f8694ea1d1
yuhangdo yuhangdo <wangyuh111@163.com>
2022-09-26 22:01:17 +0800

    finish iterator

--- deque.h
+++ deque.h
@@ -1,7 +1,7 @@
 #ifndef _YHSTL_DEQUE_H_
 #define _YHSTL_DEQUE_H_
 //20220924  今日开始deque容器实现  先做部分迭代器
-
+//20220926  最近需要准备开题，finish 迭代器部分
 #include"iterator.h"
 
 namespace yhstl
@@ -35,7 +35,99 @@ namespace yhstl
 		T* cur;
 		T* first;
 		T* last;
-		map_pointer map;   //指向管控中心
+		map_pointer node;   //指向管控中心
+
+
+		//先加一个跳缓冲区的函数   跳一个缓冲区大小
+		void set_node(map_pointer new_node)
+		{
+			node = new_node;
+			first = *new_node;
+			last = first + difference_type(buffer_size());
+		}
+
+		//接下来是迭代器的各种重载运算符操作
+		//* 和 -> 运算符是老程序了
+		reference operator*() { return *cur; }   
+		pointer operator->() const { return &(operator*()); }
+
+		difference_type operator -(self& x)const    //这个重载-的逻辑挺有意思
+		{
+			return difference_type(buffer_size()) * (node - x.node - 1) + (cur - first) + (x.last - x.cur);
+		}
+		self& operator++()    //要特判一下 在缓冲区边界的情况
+		{
+			++cur;
+			if (cur == last)
+			{
+				set_node(node + 1);
+				cur = first;
+			}
+			return *this;
+		}
+		self& operator++(int)   //后置++    后置调前置 标准写法
+		{
+			self tmp = *this;
+			++* this;
+			return tmp;
+		}
+		self& operator--()    //要特判一下 在缓冲区边界的情况
+		{		
+			if (cur == first)
+			{
+				set_node(node -1);
+				cur = last;
+			}
+			--cur;
+			return *this;
+		}
+		self& operator--(int)   //后置--    后置调前置 标准写法
+		{
+			self tmp = *this;
+			--* this;
+			return tmp;
+		}
+
+		//因为是随机迭代器，支持随机存取，所以要实现一下
+		self& operator+=(difference_type n)
+		{
+			difference_type offset = n + (cur - first);   //先算出跳跃距离
+			if (offset >= 0 && offset < difference_type(buffer_size()))  //判断是不是在同一个缓冲区内
+			{
+				cur += n;
+			}
+			else
+			{
+				difference_type node_offset = offset > 0 ? offset / difference_type(buffer_size()) : -difference_type((-offset-1) /(buffer_size())-1;
+				//切换到正确的节点
+				set_node(node + node_offset);
+				//切换到正确的元素
+				cur = first + (offset - node_offset * difference_type(buffer_size()));
+			}
+			return *this;
+		}
+		self operator+(difference_type  n)const   //迭代器的+
+		{
+			self tmp = *this;
+			return tmp += n;
+		}
+		self operator-=(difference_type n) { return *this += -n };
+		self operator-(difference_type  n)const   //迭代器的-
+		{
+			self tmp = *this;
+			return tmp -= n;
+		}
+
+		//以下实现随机存取  调用 operator+
+		reference operator[](difference_type n) const { return *(*this + n); }
+		
+		//以下调用 operator* operator+
+		bool operator == (const self& x) { return cur == x.cur; }
+		bool operator != (const self& x) { return !(*this == x); }
+		bool operator<(const self& x)const
+		{
+			return (node == x.node) ? (cur < x.cur) : (node < x.node);
+		}
 	};
 	
 }
